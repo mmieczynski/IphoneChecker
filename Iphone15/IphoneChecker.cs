4787fce36227b87c6cdf00429451ac3e7eeecaef
Maciej <maciej@Maciejs-MacBook-Air.local>
2023-12-10 02:40:18 +0100

    log more

--- Iphone15/IphoneChecker.cs
+++ Iphone15/IphoneChecker.cs
@@ -14,7 +14,7 @@ public class IphoneChecker(HttpClient client, IConfiguration configuration) : Ba
     {
         Log.Information("Starting iphoner service!");
         const string accountSid = "AC15105eeccd558f6a2820d62a128589e2";
-        Log.Information(Environment.GetEnvironmentVariable("TwilioToken"));
+        Log.Information($"Twilio token: {Environment.GetEnvironmentVariable("TwilioToken")}");
         var authToken = Environment.GetEnvironmentVariable("TwilioToken");
         TwilioClient.Init(accountSid, authToken ?? throw new InvalidOperationException());
         
